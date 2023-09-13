4d2344e603202fc4b239e00b24884e19eebeea6a
aleramirezsj <aleramirezsj@gmail.com>
2023-09-13 15:03:30 -0300

    obtención de libros funcionando

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 console.log("el archivo javascript ya está importado");
 
-const urlApi="https://biblioisp20-92ed.restdb.io/rest/libros?apikey=2c677ccf2cb62a940092248e128001983dab0";
+const urlApi="https://biblioisp20-92ed.restdb.io/rest/libros?apikey=64f8c70b68885429270bfe7d";
 
-const libros = {
+const applibros = {
     listarLibros: ()=>{
         //tomamos la referencia del contenedor donde se mostrarán los libros
         const contenedor=document.getElementById("contenedorLibros");
-        const books="<ul><li> Libro1 <li>Libro2 </ul>";
-        contenedor.innerHTML=books;
+        
+        //cremos una variable vacía que contendrá todo el código HTML que vamos a insertar
+        let contenidoHTML = "";
+        
+        fetch(urlApi)
+        .then(respuesta=>respuesta.json())
+        .then(libros=>{
+            console.log(libros);
+
+            for (const libro of libros) {
+                contenidoHTML += `
+                <div>
+                    <img src="${libro.portada_url}" class="img-thumbnail"/>
+                    <details>
+                        <summary>${libro.nombre}</summary>
+                        ${libro.sinopsis}
+                    </details>
+
+                </div>
+                `;
+            };
+            contenedor.innerHTML=contenidoHTML;
+        })
+        
     }
 }
-libros.listarLibros();
+applibros.listarLibros();
