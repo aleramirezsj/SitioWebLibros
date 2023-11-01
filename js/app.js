1b0e67334cae75f0e50c4fa1d9f831732d1a2e02
aleramirezsj <aleramirezsj@gmail.com>
2023-11-01 14:54:40 -0300

    agregamos el botón de eliminar película

--- js/app.js
+++ js/app.js
@@ -29,6 +29,7 @@ const applibros = {
                         Editorial:${libro.editorial}
                     </details>
                     <a href="#" onclick="applibros.editarLibro('${libro._id}')">Editar</a>
+                    <a href="#" onclick="applibros.eliminarLibro('${libro._id}','${libro.nombre}')">Eliminar</a>
 
                 </div>
                 `;
@@ -37,7 +38,38 @@ const applibros = {
         })
         
     },
-    agregarLibro: ()=>{
+    eliminarLibro: (idAEliminar,nombreABorrar)=>{
+      Swal.fire({
+        title: `¿Está seguro que desea borrar al libro ${nombreABorrar}`,
+        text: "No podrás revertir esta operación",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Si, quiero borrarlo!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const urlApi=`https://biblioisp20-92ed.restdb.io/rest/libros/${idAEliminar}?apikey=64f8c70b68885429270bfe7d`
+          //`https://pracprof2023-af4f.restdb.io/rest/peliculas/${idPeliculaBorrar}?apikey=6467b09a0b60fc42f4e197fa`
+      fetch(urlApi, {
+        method: 'DELETE'
+        })
+        .then(response => {
+          console.log(response);
+          return applibros.listarLibros();
+        }).then(response =>{
+          Swal.fire(
+            'Eliminado!',
+            `El libro ${nombreABorrar} fue borrado .`,
+            'satisfactoriamente'
+          )
+        });
+        }
+      })
+    },
+    guardarLibro: ()=>{
+        const txtId=document.getElementById("txtId");
         const txtNombre=document.getElementById("txtNombre");
         const txtPaginas=document.getElementById("txtPaginas");
         const txtAutor=document.getElementById("txtAutor");
@@ -45,8 +77,19 @@ const applibros = {
         const txtPortadaUrl=document.getElementById("txtPortadaUrl");
         const txtSinopsis=document.getElementById("txtSinopsis");
         const txtGenero=document.getElementById("txtGenero");
-        
-        const nuevoLibro = {
+
+        let urlApi='';
+        let metodoHttp='';
+        if(txtId.value==='')
+        {
+          urlApi="https://biblioisp20-92ed.restdb.io/rest/libros?apikey=64f8c70b68885429270bfe7d";
+          metodoHttp='POST';
+        }else{
+          urlApi=`https://biblioisp20-92ed.restdb.io/rest/libros/${txtId.value}?apikey=64f8c70b68885429270bfe7d`;
+          metodoHttp='PUT';
+        }
+
+        const libroAGuardar = {
             "nombre": txtNombre.value,
             "paginas": txtPaginas.value,
             "autor": txtAutor.value,
@@ -55,13 +98,13 @@ const applibros = {
             "sinopsis": txtSinopsis.value,
             "genero": txtGenero.value
           };  
-        console.log(nuevoLibro);  
+        console.log(libroAGuardar);  
         fetch(urlApi, {
-            method: "POST",
+            method: metodoHttp,
             headers: {
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify(nuevoLibro)
+            body: JSON.stringify(libroAGuardar)
             })  
             .then(response => {
               console.log(response);
@@ -75,8 +118,18 @@ const applibros = {
         fetch(urlApi
             ).then(res => res.json())
               .then(libro => {
+                document.getElementById("txtId").value=libro._id;
                 document.getElementById("txtNombre").value=libro.nombre;
                 document.getElementById("txtPaginas").value=libro.paginas;
+                document.getElementById("txtAutor").value=libro.autor;
+                document.getElementById("txtEditorial").value=libro.editorial;
+                document.getElementById("txtPortadaUrl").value=libro.portada_url;
+                document.getElementById("txtSinopsis").value=libro.sinopsis;
+                document.getElementById("txtGenero").value=libro.genero;
+
+                const ventanaEditar=document.getElementById('agregarEditarModal');
+                let ventana = new bootstrap.Modal(ventanaEditar);
+                ventana.show();
 
               });
     }
